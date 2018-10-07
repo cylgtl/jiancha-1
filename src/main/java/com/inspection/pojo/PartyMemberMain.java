3d65114ffa817884985c586a0f37896b897b93b4
changwenlong <786320366@qq.com>
2018-10-07 13:31:14 +0800

    修改入党申请书类型

--- src/main/java/com/inspection/pojo/PartyMemberMain.java
+++ src/main/java/com/inspection/pojo/PartyMemberMain.java
@@ -1,5 +1,7 @@
 package com.inspection.pojo;
 import com.inspection.entity.partyMember.PartyMemberEntity;
+
+import java.util.Date;
 import java.util.List;
 public class PartyMemberMain {
     private PartyMemberEntity entity;
@@ -10,7 +12,7 @@ public class PartyMemberMain {
         this.entity = entity;
     }
     //入党申请书
-    private String ruDangShenQing;
+    private byte[] ruDangShenQing;
     //培养联系人
     private String lianXiRen1;
     private String lianXiRen2;
@@ -25,18 +27,20 @@ public class PartyMemberMain {
     private int zanCheng; //赞成票
     private String testRate; //得票率
     //组织审批
-    private ZuZhiShenPi ruDangJiJi; //确认入党积极分子研究决定
-    private ZuZhiShenPi dangYuanFaZhan; //确认党员发展
-    private ZuZhiShenPi yuBeiDangYuan; //接受预备党员研究决定
-    private ZuZhiShenPi dangWei; //党委研究决定
-    private ZuZhiShenPi yuBeiZhuanZheng; //预备党员转正
+    private Date ruDangJiJiTime; //确认入党积极分子研究决定时间
+    private String ruDangJiJi; //确认入党积极分子研究决定
+    private Date dangYuanFaZhanTime; //确认党员发展时间
+    private String dangYuanFaZhan; //确认党员发展
+    private Date yuBeiDangYuanTime; //接受预备党员研究决定时间
+    private String yuBeiDangYuan; //接受预备党员研究决定
+    private Date dangWeiTime; //党委研究决定时间
+    private String dangWei; //党委研究决定
+    private Date yuBeiZhuanZhengTime; //预备党员转正时间
+    private String yuBeiZhuanZheng; //预备党员转正
     //军事训练成绩
     private List<JunShiXunLian> junShiXunLian;
     //表彰和获奖情况
     private List<String> biaoZhang;
-    public void setRuDangShenQing(String ruDangShenQing) {
-        this.ruDangShenQing = ruDangShenQing;
-    }
     public void setLianXiRen1(String lianXiRen1) {
         this.lianXiRen1 = lianXiRen1;
     }
@@ -67,26 +71,148 @@ public class PartyMemberMain {
     public void setTestRate(String testRate) {
         this.testRate = testRate;
     }
-    public void setRuDangJiJi(ZuZhiShenPi ruDangJiJi) {
-        this.ruDangJiJi = ruDangJiJi;
-    }
-    public void setDangYuanFaZhan(ZuZhiShenPi dangYuanFaZhan) {
-        this.dangYuanFaZhan = dangYuanFaZhan;
-    }
-    public void setYuBeiDangYuan(ZuZhiShenPi yuBeiDangYuan) {
-        this.yuBeiDangYuan = yuBeiDangYuan;
-    }
-    public void setDangWei(ZuZhiShenPi dangWei) {
-        this.dangWei = dangWei;
-    }
-    public void setYuBeiZhuanZheng(ZuZhiShenPi yuBeiZhuanZheng) {
-        this.yuBeiZhuanZheng = yuBeiZhuanZheng;
-    }
+
     public void setJunShiXunLian(List<JunShiXunLian> junShiXunLian) {
         this.junShiXunLian = junShiXunLian;
     }
     public void setBiaoZhang(List<String> biaoZhang) {
         this.biaoZhang = biaoZhang;
+    }
+
+    public String getLianXiRen1() {
+        return lianXiRen1;
+    }
+
+    public String getLianXiRen2() {
+        return lianXiRen2;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public int getYingDao() {
+        return yingDao;
+    }
+
+    public int getShiDao() {
+        return shiDao;
+    }
+
+    public int getYouXiao() {
+        return youXiao;
+    }
+
+    public String getChuQin() {
+        return chuQin;
+    }
+
+    public int getZanCheng() {
+        return zanCheng;
+    }
+
+    public String getTestRate() {
+        return testRate;
+    }
+
+    public Date getRuDangJiJiTime() {
+        return ruDangJiJiTime;
+    }
+
+    public void setRuDangJiJiTime(Date ruDangJiJiTime) {
+        this.ruDangJiJiTime = ruDangJiJiTime;
+    }
+
+    public String getRuDangJiJi() {
+        return ruDangJiJi;
+    }
+
+    public void setRuDangJiJi(String ruDangJiJi) {
+        this.ruDangJiJi = ruDangJiJi;
+    }
+
+    public Date getDangYuanFaZhanTime() {
+        return dangYuanFaZhanTime;
+    }
+
+    public void setDangYuanFaZhanTime(Date dangYuanFaZhanTime) {
+        this.dangYuanFaZhanTime = dangYuanFaZhanTime;
+    }
+
+    public String getDangYuanFaZhan() {
+        return dangYuanFaZhan;
+    }
+
+    public void setDangYuanFaZhan(String dangYuanFaZhan) {
+        this.dangYuanFaZhan = dangYuanFaZhan;
+    }
+
+    public Date getYuBeiDangYuanTime() {
+        return yuBeiDangYuanTime;
+    }
+
+    public void setYuBeiDangYuanTime(Date yuBeiDangYuanTime) {
+        this.yuBeiDangYuanTime = yuBeiDangYuanTime;
+    }
+
+    public String getYuBeiDangYuan() {
+        return yuBeiDangYuan;
+    }
+
+    public void setYuBeiDangYuan(String yuBeiDangYuan) {
+        this.yuBeiDangYuan = yuBeiDangYuan;
+    }
+
+    public Date getDangWeiTime() {
+        return dangWeiTime;
+    }
+
+    public void setDangWeiTime(Date dangWeiTime) {
+        this.dangWeiTime = dangWeiTime;
+    }
+
+    public String getDangWei() {
+        return dangWei;
+    }
+
+    public void setDangWei(String dangWei) {
+        this.dangWei = dangWei;
+    }
+
+    public Date getYuBeiZhuanZhengTime() {
+        return yuBeiZhuanZhengTime;
+    }
+
+    public void setYuBeiZhuanZhengTime(Date yuBeiZhuanZhengTime) {
+        this.yuBeiZhuanZhengTime = yuBeiZhuanZhengTime;
+    }
+
+    public String getYuBeiZhuanZheng() {
+        return yuBeiZhuanZheng;
+    }
+
+    public void setYuBeiZhuanZheng(String yuBeiZhuanZheng) {
+        this.yuBeiZhuanZheng = yuBeiZhuanZheng;
+    }
+
+    public List<JunShiXunLian> getJunShiXunLian() {
+        return junShiXunLian;
+    }
+
+    public List<String> getBiaoZhang() {
+        return biaoZhang;
+    }
+
+    public byte[] getRuDangShenQing() {
+        return ruDangShenQing;
+    }
+
+    public void setRuDangShenQing(byte[] ruDangShenQing) {
+        this.ruDangShenQing = ruDangShenQing;
     }
 }
 class JunShiXunLian {
